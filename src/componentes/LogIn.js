1a0ad84c3d11254605e2babcbd137f73d9bfba14
Pou <paggustintomas@gmail.com>
2024-04-23 17:13:28 -0300

    Cambie una boludez de un hoja css

--- src/componentes/LogIn.js
+++ src/componentes/LogIn.js
@@ -6,7 +6,7 @@ import { RiLockPasswordFill } from "react-icons/ri";
 import '../estilos/footer.css'
 function LogIn() {
 
-console.log("Juancho")
+
 
   const [action,setAction] = useState("Login");
 
