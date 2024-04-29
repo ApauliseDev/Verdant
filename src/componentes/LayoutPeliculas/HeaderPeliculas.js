836e0dcefb07dcd46614b3122dfc1fb56f9e0343
“Thiago” <thiagoargschool@gmail.com>
2024-04-29 02:00:33 -0300

    modifique el layoutPeliculas, agregue colores e informacion

--- src/componentes/LayoutPeliculas/HeaderPeliculas.js
+++ src/componentes/LayoutPeliculas/HeaderPeliculas.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "../../estilos/headerPeliculas.css";
 import RatingStars from "./RatingStars";
+
+import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AlarmIcon from '@mui/icons-material/Alarm';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
 function HeaderPeliculas() {
   return (
     <header className="header">
@@ -16,8 +23,37 @@ function HeaderPeliculas() {
       </div>
       <article className="movie-info">
       <h2>Godzilla x Kong </h2>
+      <div className="iconos">
+        <IconButton aria-label="delete">
+          <DeleteIcon />
+        </IconButton>
+        <IconButton aria-label="delete" disabled color="primary">
+          <DeleteIcon />
+        </IconButton>
+        <IconButton color="secondary" aria-label="add an alarm">
+          <AlarmIcon />
+        </IconButton>
+        <IconButton color="primary" aria-label="add to shopping cart">
+          <AddShoppingCartIcon />
+        </IconButton>
+      </div>
       <h6> Descripción general</h6>
-      <p>Po is gearing up to become the spiritual leader of his Valley of Peace, but also needs someone to take his place as Dragon Warrior. As such, he will train a new kung fu practitioner for the spot and will encounter a villain called the Chameleon who conjures villains from the past</p>
+      <p>Una aventura cinematográfica completamente nueva, que enfrentará al todopoderoso Kong y al temible Godzilla contra una colosal amenaza desconocida escondida dentro de nuestro mundo. La nueva y épica película profundizará en las historias de estos titanes, sus orígenes y los misterios de Isla Calavera y más allá, mientras descubre la batalla mítica que ayudó a forjar a estos seres extraordinarios y los unió a la humanidad para siempre.</p>
+      <div className="reparto">
+        <div>
+          <h5>Director</h5>
+          <p>Mariano</p>
+        </div>
+        <div>
+          <h5>Productor</h5>
+          <p>Marcelo</p>
+        </div>
+        <div>
+          <h5>Ayudante</h5>
+          <p>Martin</p>
+        </div>
+      </div>
+
 
       </article>
       </section>
--- src/componentes/LayoutPeliculas/RatingStars.js
+++ src/componentes/LayoutPeliculas/RatingStars.js
@@ -4,16 +4,16 @@ import Box from '@mui/material/Box';
 import StarIcon from '@mui/icons-material/Star';
 
 const labels = {
-  0.5: 'Useless',
-  1: 'Useless+',
-  1.5: 'Poor',
-  2: 'Poor+',
-  2.5: 'Ok',
-  3: 'Ok+',
-  3.5: 'Good',
-  4: 'Good+',
-  4.5: 'Excellent',
-  5: 'Excellent+',
+  0.5: 'Mala',
+  1: 'Mala',
+  1.5: 'Okay',
+  2: 'Okay',
+  2.5: 'Buena',
+  3: 'Buena',
+  3.5: 'Genial',
+  4: 'Genial',
+  4.5: 'Legendaria',
+  5: 'Legendaria',
 };
 
 function getLabelText(value) {
@@ -33,6 +33,7 @@ export default function RatingStars() {
       }}
     >
       <Rating
+        sx={{color: "#5bd635"}}
         name="hover-feedback"
         value={value}
         precision={0.5}
@@ -43,10 +44,10 @@ export default function RatingStars() {
         onChangeActive={(event, newHover) => {
           setHover(newHover);
         }}
-        emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+        emptyIcon={<StarIcon style={{ opacity: 0.55}} fontSize="inherit" />}
       />
       {value !== null && (
-        <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : value]}</Box>
+        <Box sx={{ ml: 2, color: "#5bd635", fontWeight:"bold"}}>{labels[hover !== -1 ? hover : value]}</Box>
       )}
     </Box>
   );
